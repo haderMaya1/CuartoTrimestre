f9f7fa6ae390bef79140c974e431f665e5de7a26
haderMaya1 <138158900+haderMaya1@users.noreply.github.com>
2023-10-16 09:48:09 -0500

    Add files via upload

---
+++ Python/store/models.py
@@ -0,0 +1,40 @@
+from this import s
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+    def get_absolute_url(self):
+        return reverse('store:careegory_list', args=[self.slug])
+
+    def _str_(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created',)
+
+    def _str_(self):
+        return self.title
